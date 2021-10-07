6f07e9a86264d1ccc609883ac40686a3cb3153b0
João de Queiroz <joao.queiroz@modalgr.com.br>
2021-10-07 10:15:28 -0300

    Repository plus entity framework

---
+++ AspNetAPIProject02.Data/Contexts/SqlServerContext.cs
@@ -0,0 +1,28 @@
+﻿using AspNetAPIProject02.Data.Entities;
+using AspNetAPIProject02.Data.Mappings;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetAPIProject02.Data.Contexts
+{
+    //Inherit DbContext
+    public class SqlServerContext :DbContext
+    {
+        public SqlServerContext(DbContextOptions<SqlServerContext> options) : base(options)
+        {
+
+        }
+        //Need to declare DbSet for each entity
+        public DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Need to add each mapping class in the project 
+            modelBuilder.ApplyConfiguration(new UserMapping());
+        }
+    }
+}
---
+++ AspNetAPIProject02.Data/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetAPIProject02.Data.Entities
+{
+    public class User
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+    }
+}
