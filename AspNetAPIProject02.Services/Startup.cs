1620ea734fd81a5a53b788a34dd9ae1c8a480864
joaogqueiroz <joaogilberto350@hotmail.com>
2024-04-20 16:46:16 -0300

    feat: update

--- AspNetAPIProject02.Services/Startup.cs
+++ AspNetAPIProject02.Services/Startup.cs
@@ -1,12 +1,12 @@
 using AspNetAPIProject02.Services.Configurations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
